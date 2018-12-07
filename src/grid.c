6d56485c557ea72b32603ed20fec58b141c10eb0
Rene Lafleur <rene@tracktik.com>
2018-12-07 13:26:47 -0500

    Allow debugging the grid from GNU/Linux console

--- src/grid.c
+++ src/grid.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "grid.h"
 #include "macros.h"
 
@@ -6,12 +5,10 @@
 // That gives us an area of 28x24 tiles.
 // Each individual tile is used to display a 2x2 block of cells.
 // This gives us an effective cell grid of 56x48.
-//#define GRID_COLS 56
-//#define GRID_ROWS 48
-#define GRID_COLS 10 
-#define GRID_ROWS 10
-#define PADDING_COLS 2
-#define PADDING_ROWS 2
+#define GRID_COLS 56
+#define GRID_ROWS 48
+#define PADDING_COLS 3 // 2 bits on the left side, 1 bit on the right
+#define PADDING_ROWS 2 // 2 entirely unused rows (one at the top and one at the bottom)
 #define BITS_IN_BYTE 8
 
 #define GRID_ARR_BYTES_RESERVED_FOR_ROW (ceil_div(GRID_COLS + PADDING_COLS, BITS_IN_BYTE))
@@ -23,9 +20,7 @@ static uint8_t grid_buffer2[GRID_BUFFER_SIZE_BYTES];
 static uint8_t *current_grid = grid_buffer1;
 static uint8_t *work_grid    = grid_buffer2;
 
-uint8_t grid_count_cell_neighbors(uint8_t *ptr, uint8_t bitmask);
-
-void /*__fastcall__*/ grid_buffer_swap(void)
+void __fastcall__ grid_buffer_swap(void)
 {
     uint8_t *ptr;
 
@@ -34,13 +29,13 @@ void /*__fastcall__*/ grid_buffer_swap(void)
     work_grid    = ptr;
 }
 
-void /*__fastcall__*/ grid_set_cell(uint8_t col, uint8_t row, cell_status_t cell_status)
+void __fastcall__ grid_set_cell(uint8_t col, uint8_t row, cell_status_t cell_status)
 {
     uint8_t *ptr = current_grid + GRID_ARR_BYTES_RESERVED_FOR_ROW; // We skip the array's first row (padding)
     uint8_t skip_row_bytes, bit_to_read, arr_column;
     uint8_t bitmask = 0x80;
 
-    arr_column = col + 1; // First column of the array is skipped (padding)
+    arr_column = col + 2; // First two columns of the array are skipped (padding)
 
     // Point to requested row
     ptr += row * (uint8_t)GRID_ARR_BYTES_RESERVED_FOR_ROW;
@@ -58,57 +53,7 @@ void /*__fastcall__*/ grid_set_cell(uint8_t col, uint8_t row, cell_status_t cell
     }
 }
 
-void /*__fastcall__*/ grid_work(void)
-{
-    uint8_t row_count, col_count;
-    uint8_t bitmask, cell_is_occupied, neighbor_count;
-    uint8_t *row_ptr, *col_ptr, *work_grid_ptr;
-
-    row_ptr = current_grid + GRID_ARR_BYTES_RESERVED_FOR_ROW; // We skip the array's first row (padding)
-
-    for (row_count = 0; row_count < GRID_ROWS; row_count++) {
-        // Bitmask targets the second bit from the left since the first column of the row is skipped (padding).
-        bitmask = 0x40;
-        col_ptr = row_ptr;
-
-        for (col_count = 0; col_count < GRID_COLS; col_count++) {
-            cell_is_occupied = *col_ptr & bitmask;
-            neighbor_count = grid_count_cell_neighbors(col_ptr, bitmask);    
-
-            // Get a pointer to the same cell position in the work grid.
-            work_grid_ptr = work_grid + (col_ptr - current_grid);
-
-            // Apply the 3 basic rules of "Game Of Life"
-            if (cell_is_occupied) {
-                if (neighbor_count < 2 || neighbor_count > 3) {
-                    *work_grid_ptr &= ~bitmask;
-                } else {
-                    *work_grid_ptr |= bitmask;
-                }
-            } else {
-                if (neighbor_count == 3) {
-                    *work_grid_ptr |= bitmask;
-                } else {
-                    *work_grid_ptr &= ~bitmask;
-                }
-            }
-            
-            bitmask = bitmask >> 1;
-
-            if (!bitmask) {
-                // Move on to next byte.
-                bitmask = 0x80;
-                col_ptr++;
-            }
-        }
-
-        row_ptr += GRID_ARR_BYTES_RESERVED_FOR_ROW;
-    }
-
-    grid_buffer_swap(); // Work buffer now becomes our current grid.
-}
-
-uint8_t grid_count_cell_neighbors(uint8_t *ptr, uint8_t bitmask)
+uint8_t __fastcall__ grid_count_cell_neighbors(uint8_t *ptr, uint8_t bitmask)
 {
     uint8_t *ptr_copy;
     uint8_t bitmask_copy;
@@ -165,3 +110,93 @@ uint8_t grid_count_cell_neighbors(uint8_t *ptr, uint8_t bitmask)
 
     return neighbor_count;
 }
+
+void __fastcall__ grid_apply_rules(void)
+{
+    uint8_t row_count, col_count;
+    uint8_t bitmask, cell_is_occupied, neighbor_count;
+    uint8_t *row_ptr, *col_ptr, *work_grid_ptr;
+
+    row_ptr = current_grid + GRID_ARR_BYTES_RESERVED_FOR_ROW; // We skip the array's first row (padding)
+
+    for (row_count = 0; row_count < GRID_ROWS; row_count++) {
+        // Bitmask targets the third bit from the left since the two first columns of the row are skipped (padding).
+        bitmask = 0x20;
+        col_ptr = row_ptr;
+
+        for (col_count = 0; col_count < GRID_COLS; col_count++) {
+            cell_is_occupied = *col_ptr & bitmask;
+            neighbor_count = grid_count_cell_neighbors(col_ptr, bitmask);    
+
+            // Get a pointer to the same cell position in the work grid.
+            work_grid_ptr = work_grid + (col_ptr - current_grid);
+
+            // Apply the 3 basic rules of "Game Of Life"
+            if (cell_is_occupied) {
+                if (neighbor_count < 2 || neighbor_count > 3) {
+                    *work_grid_ptr &= ~bitmask;
+                } else {
+                    *work_grid_ptr |= bitmask;
+                }
+            } else {
+                if (neighbor_count == 3) {
+                    *work_grid_ptr |= bitmask;
+                } else {
+                    *work_grid_ptr &= ~bitmask;
+                }
+            }
+            
+            bitmask = bitmask >> 1;
+
+            if (!bitmask) {
+                // Move on to next byte.
+                bitmask = 0x80;
+                col_ptr++;
+            }
+        }
+
+        row_ptr += GRID_ARR_BYTES_RESERVED_FOR_ROW;
+    }
+
+    grid_buffer_swap(); // Work buffer now becomes our current grid.
+}
+
+#ifdef _DEBUG_
+void grid_display_on_stdout(char occupied_char, char free_char)
+{
+    uint8_t row_count, col_count;
+    uint8_t bitmask, cell_is_occupied;
+    uint8_t *row_ptr, *col_ptr;
+
+    row_ptr = current_grid + GRID_ARR_BYTES_RESERVED_FOR_ROW; // We skip the array's first row (padding)
+
+    for (row_count = 0; row_count < GRID_ROWS; row_count++) {
+        // Bitmask targets the third bit from the left since the two first columns of the row are skipped (padding).
+        bitmask = 0x20;
+        col_ptr = row_ptr;
+
+        for (col_count = 0; col_count < GRID_COLS; col_count++) {
+            cell_is_occupied = *col_ptr & bitmask;
+
+            // Apply the 3 basic rules of "Game Of Life"
+            if (cell_is_occupied) {
+                printf("%c", occupied_char);
+            } else {
+                printf("%c", free_char);
+            }
+            
+            bitmask = bitmask >> 1;
+
+            if (!bitmask) {
+                // Move on to next byte.
+                bitmask = 0x80;
+                col_ptr++;
+            }
+        }
+
+        printf("\n");
+
+        row_ptr += GRID_ARR_BYTES_RESERVED_FOR_ROW;
+    }
+}
+#endif
--- src/grid.h
+++ src/grid.h
@@ -3,8 +3,18 @@
 
 #include <stdint.h>
 
+#ifdef _DEBUG_
+  // We are compiling on a GNU/Linux host
+  #include <stdio.h>
+  #define __fastcall__
+#endif
+
 typedef enum { CELL_FREE, CELL_OCCUPIED } cell_status_t;
 
-void /*__fastcall__*/ grid_set_cell(uint8_t col, uint8_t row, cell_status_t cell_status);
+void __fastcall__ grid_set_cell(uint8_t col, uint8_t row, cell_status_t cell_status);
+void __fastcall__ grid_apply_rules(void);
 
+#ifdef _DEBUG_
+void grid_display_on_stdout(char occupied_char, char free_char);
 #endif
+#endif // _GRID_H_
