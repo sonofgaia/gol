89555a5f4dba45d7a6750b32195606bfe013392b
Rene Lafleur <rene@tracktik.com>
2018-12-08 08:39:17 -0500

    Better timing for test

--- src/tests/grid_test.c
+++ src/tests/grid_test.c
@@ -1,22 +1,33 @@
 #include "grid.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
+    struct timespec ts1, ts2;
+
+    ts1.tv_sec = 0;
+    ts1.tv_nsec = 500000000;
+
     grid_set_cell(20, 10, CELL_OCCUPIED);
     grid_set_cell(21, 10, CELL_OCCUPIED);
-//    grid_set_cell(20, 21, CELL_OCCUPIED);
-//    grid_set_cell(21, 21, CELL_OCCUPIED);
-    grid_set_cell(22, 10, CELL_OCCUPIED);
-    grid_set_cell(23, 10, CELL_OCCUPIED);
-    grid_set_cell(24, 10, CELL_OCCUPIED);
+    grid_set_cell(21, 11, CELL_OCCUPIED);
+    grid_set_cell(22, 11, CELL_OCCUPIED);
+    grid_set_cell(22, 12, CELL_OCCUPIED);
+    grid_set_cell(23, 12, CELL_OCCUPIED);
+
+    grid_set_cell(20, 30, CELL_OCCUPIED);
+    grid_set_cell(21, 30, CELL_OCCUPIED);
+    grid_set_cell(22, 30, CELL_OCCUPIED);
+    grid_set_cell(23, 30, CELL_OCCUPIED);
+    grid_set_cell(24, 30, CELL_OCCUPIED);
 
     while (1) {
         grid_display_on_stdout('+', ' ');
         grid_apply_rules(); 
         printf("---------------------------------\n");
-        sleep(1);
+        nanosleep(&ts1, &ts2);
     }
 
     return 0;
