9c624c397641d94440b988950ab9100ffa5a1066
Rene Lafleur <rene@tracktik.com>
2018-12-24 10:12:14 -0500

    Replaced ASM code with C code for MMC3

--- src/core/mmc3.c
+++ src/core/mmc3.c
@@ -1,6 +1,10 @@
 #include <stdint.h>
 #include "mmc3.h"
 
+#define MMC3_BANK_SELECT     *((uint8_t*)0x8000)
+#define MMC3_BANK_DATA       *((uint8_t*)0x8001)
+#define MMC3_PRG_RAM_PROTECT *((uint8_t*)0xA001)
+
 typedef struct {
     uint16_t bank_select   :3;
     uint16_t unused        :3;
@@ -8,9 +12,7 @@ typedef struct {
     uint16_t chr_bank_mode :1;
 } mmc3_bank_select_t;
 
-// Functions and data defined in 'ppu.asm'
-extern mmc3_bank_select_t mmc3_bank_select;
-#pragma zpsym ("mmc3_bank_select");
+static mmc3_bank_select_t mmc3_bank_select;
 
 void __fastcall__ mmc3_set_prg_bank_mode(mmc3_prg_bank_mode_t prg_bank_mode)
 {
@@ -27,8 +29,20 @@ void __fastcall__ mmc3_switch_bank(mmc3_bank_reg_t bank_reg, uint8_t bank_num)
     mmc3_bank_select.bank_select = bank_reg;
 
     // Select bank register
-    *((uint8_t*)0x8000) = (uint8_t)mmc3_bank_select;
+    MMC3_BANK_SELECT = (uint8_t)mmc3_bank_select;
 
-    // Change bank register
-    *((uint8_t*)0x8001) = bank_num;
+    // Change ROM bank
+    MMC3_BANK_DATA = bank_num;
+}
+
+void __fastcall__ mmc3_enable_prg_ram(void)
+{
+    // Bit 7 'on' activates the RAM chip
+    MMC3_PRG_RAM_PROTECT = 0x80;
+}
+
+void __fastcall__ mmc3_disable_prg_ram(void)
+{
+    // Bit 7 'off' disables the RAM chip, accesses to $6000-$7FFFF will return 'open bus'.
+    MMC3_PRG_RAM_PROTECT = 0x00;
 }
